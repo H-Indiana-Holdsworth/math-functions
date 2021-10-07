d046eaa0458041608a51577aa93be961f56bfdb6
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-07 13:24:20 -0700

    passing test for problem 2

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,9 +12,7 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-    const sum = a + b;
-    const str = `The sum of ${a} and ${b} is ${sum}.`;
-    return [sum, str];
+
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
@@ -28,7 +26,10 @@ Write a function called multiply() that takes in two numbers as arguments and re
 */
 
 export function multiply(a, b) {
+    const product = a * b;
+    const str = `The product of ${a} and ${b} is ${product}.`;
 
+    return [product, str];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
